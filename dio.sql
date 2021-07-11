2cd0ecb43b6bfd1ebfcf09a5a3f18799f2d224bc
estevaodeabreu <estevaodeabreu.gti@gmail.com>
2021-07-11 13:44:39 -0300

    primeira tabela Mysql

--- dio.sql
+++ dio.sql
@@ -6,3 +6,4 @@ CREATE TABLE pessoas (
 INSERT INTO pessoas('nome','nascimento') VALUES ('Estevão de Abreu','1985-01-22')
 INSERT INTO pessoas('nome','nascimento') VALUES ('Ronaldo Nogueira','1986-01-22')
 INSERT INTO pessoas('nome','nascimento') VALUES ('Eduarda Rudolph','2000-04-12')
+INSERT INTO pessoas('nome','nascimento') VALUES ('Rejane Cristina de Abreu Machado','1970-05-11')
